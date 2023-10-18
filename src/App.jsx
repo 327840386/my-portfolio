f3fa250006a0a631342a95d05b15e71ab4a51aad
Yuhao Lu <101802245+327840386@users.noreply.github.com>
2023-10-18 04:24:54 +0000

    set homepage for github pages deploy

--- src/App.jsx
+++ src/App.jsx
@@ -24,8 +24,8 @@ import "./styles.css";
  * If you don't have one of the social sites listed, leave it as an empty string.
  */
 const siteProps = {
-  name: "Alexandrie Grenier",
-  title: "Web Designer & Content Creator",
+  name: "Yuhao Lu",
+  title: "software engineer",
   email: "alex@example.com",
   gitHub: "microsoft",
   instagram: "microsoft",
